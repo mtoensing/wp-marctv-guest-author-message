a741a84c1a61b361e110dbdf88da2a00272db8a8
Marc Tönsing <marc@marc.tv>
2018-10-10 21:42:15 +0200

    Initial commit

---
+++ wp-marctv-guest-author-message.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Plugin Name: Guest Author Message
+ * Description: Displays a message if USP guest author is found on the post
+ * Plugin URI:  https://marc.tv
+ * Version:     0.1
+ * Text Domain: guest-author-message
+ * Domain Path: /languages
+ * GitHub Plugin URI: mtoensing/wp-marctv-guest-author-message
+ */
+
+
+function marctv_wgam_add_info($content) {
+
+	$post_id = get_the_ID();
+
+	if ( is_single() AND metadata_exists( 'post', $post_id, 'user_submit_name' )) {
+
+		$name =  get_post_meta( $post_id, 'user_submit_name', true );
+		$html = '';
+		$html .= '<p class="wgam-infobox">';
+		$html .= 'Dieser Gastartikel ist nicht von Marc sondern von ' . $name . '. Er wurde importiert von verschlichtern.de';
+		$html .= '</p>';
+
+		return $html . $content;
+
+	} else {
+		return $content;
+	}
+
+}
+
+add_filter('the_content', 'marctv_wgam_add_info');
+
+
+
+function marctv_wgam_enqueue_scripts() {
+	wp_enqueue_style(
+		"marctv-wgam", WP_PLUGIN_URL . "/wp-marctv-guest-author-message/style.css", true, "1.0");
+
+}
+
+add_action( 'wp_enqueue_scripts', 'marctv_wgam_enqueue_scripts' );
+
