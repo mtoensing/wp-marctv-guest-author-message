70233d9276a94e654a04ed434fea2eecfd01abaf
Marc Tönsing <marc@marc.tv>
2018-10-14 12:47:06 +0200

    Removed css

--- wp-marctv-guest-author-message.php
+++ wp-marctv-guest-author-message.php
@@ -3,7 +3,7 @@
  * Plugin Name: Guest Author Message
  * Description: Displays a message if USP guest author is found on the post
  * Plugin URI:  https://marc.tv
- * Version:     0.3
+ * Version:     0.4
  * Text Domain: guest-author-message
  * Domain Path: /languages
  * GitHub Plugin URI: mtoensing/wp-marctv-guest-author-message
@@ -18,7 +18,7 @@ function marctv_wgam_add_info($content) {
 
 		$name =  get_post_meta( $post_id, 'user_submit_name', true );
 		$html = '';
-		$html .= '<p class="wgam-infobox">';
+		$html .= '<p class="warning">';
 		$html .= '<strong>Hinweis:</strong> Dieser Gastartikel stammt von ' . $name . ' und wurde nicht von <a href="https://marc.tv/marc-toensing/">Marc</a> geschrieben.';
 		$html .= '</p>';
 
@@ -31,14 +31,3 @@ function marctv_wgam_add_info($content) {
 }
 
 add_filter('the_content', 'marctv_wgam_add_info');
-
-
-
-function marctv_wgam_enqueue_scripts() {
-	wp_enqueue_style(
-		"marctv-wgam", WP_PLUGIN_URL . "/wp-marctv-guest-author-message/style.css", true, "1.0");
-
-}
-
-add_action( 'wp_enqueue_scripts', 'marctv_wgam_enqueue_scripts' );
-
